5cb278edb15385a18d9e2c81db49bab1b747f83c
hamzabenjelloun1 <hamzabenjelloun77@gmail.com>
2021-06-02 07:43:55 -0400

    Deleting more unuseful files and creating some components to manage our TodoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Todolist from './Todolist'
+import React, {useState} from 'react'
 
 function App() {
-  return null
+  const [todos,setTodos] = useState(['Todo1','Todo2'])
+  return (
+    <>
+      <Todolist todos={todos}/>
+      <input type="text"/>
+      <button>Add Todo</button>
+      <button>Clear completed Todos</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
